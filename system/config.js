4a5a255b2915ce984df0178c0da937248d39348d
kaylaa-corde <116987386+kaylaa-corde@users.noreply.github.com>
2022-11-02 07:52:06 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '6282141544557'
+global.owner = '6285939048622'
 // Owner name
-global.owner_name = 'Customer Service'
+global.owner_name = 'Kaylaa'
 // Maximum upload file size limit (Default : 50 MB)
 global.max_upload = 50
 // Delay for spamming protection (Default : 3 seconds)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6282141544557@c.us'
+global.forwards = '6285939048622@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
@@ -24,7 +24,7 @@ global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Bot version
 global.version = '2.2.1',
 // Bot name
-global.botname = `© neoxr-bot v${global.version} (Public Bot)`
+global.botname = `© KayyBotz v${global.version} (Public Bot)`
 // Footer text
 global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
 // Global status
